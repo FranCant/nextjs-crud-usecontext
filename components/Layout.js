b8b9185a617abb0ae8b5eb72a08cf4fbbd80581c
Fran <cantorifrancisco@gmail.com>
2022-04-09 00:01:11 +0200

    icons add

--- components/Layout.js
+++ components/Layout.js
@@ -16,12 +16,22 @@ const Layout = ({ children }) => {
             </h1>
           </a>
         </Link>
-        <span className="ml-2 text-gray-400 font-bold">{tasks.length} Task</span>
+        <span className="ml-2 text-gray-400 font-bold">
+          {tasks.length} Task
+        </span>
         <div className="flex-grow text-right">
           <button
             className="bg-green-500 hover:bg-green-400 text-white px-5 py-2 text-gray font-bold rounded-sm inline-flex items-center"
             onClick={() => router.push("/new")}
           >
+            <svg
+              className="mr-3"
+              width="24"
+              height="24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M11 11v-11h1v11h11v1h-11v11h-1v-11h-11v-1h11z" />
+            </svg>
             Add Task
           </button>
         </div>
--- pages/index.js
+++ pages/index.js
@@ -17,19 +17,28 @@ const Home = () => {
               <div
                 key={task.id}
                 className="bg-gray-700 hover:bg-gray-600 cursor-pointer px-20 py-5 m-2 flex justify-start items-center"
-                onClick={() => push('/edit/' + task.id)}
+                onClick={() => push("/edit/" + task.id)}
               >
                 <span className="text-5xl mr-5">{i}</span>
                 <div className="w-full">
                   <div className="flex justify-between">
                     <h1 className="font-bold">{task.title}</h1>
-                    <button className="bg-red-700 hover:bg-red-600 px-3 py-1 inline-flex items-center"
-                    onClick={(e) => {
-                      e.stopPropagation();
-                      deleteTask(task.id);
-
-                    }}>
-                     
+                    <button
+                      className="bg-red-700 hover:bg-red-600 px-3 py-1 inline-flex items-center"
+                      onClick={(e) => {
+                        e.stopPropagation();
+                        deleteTask(task.id);
+                      }}
+                    >
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        width="24"
+                        height="20"
+                        viewBox="0 0 24 24"
+                        className="mr-1"
+                      >
+                        <path d="M3 6v18h18v-18h-18zm5 14c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm5 0c0 .552-.448 1-1 1s-1-.448-1-1v-10c0-.552.448-1 1-1s1 .448 1 1v10zm4-18v2h-20v-2h5.711c.9 0 1.631-1.099 1.631-2h5.315c0 .901.73 2 1.631 2h5.712z" />
+                      </svg>
                       Delete
                     </button>
                   </div>
